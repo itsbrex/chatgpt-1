3ab1b225c20050b7022a402552dacca2e6f2c406
Keito654 <keito19981129@gmail.com>
2023-03-15 02:32:10 +0900

    会話の履歴を残すように修正

--- pages/api/response.ts
+++ pages/api/response.ts
@@ -1,27 +1,48 @@
-import { OpenAIStream, OpenAIStreamPayload } from '@/utils/OpenAIStream'
+import { OpenAIStream, OpenAIStreamPayload } from "@/utils/OpenAIStream";
 
 type RequestData = {
-  currentModel: string
-  message: string
-}
+  currentModel: string;
+  message: string[];
+};
 
 if (!process.env.OPENAI_API_KEY) {
-  throw new Error('Missing env var from OpenAI')
+  throw new Error("Missing env var from OpenAI");
 }
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
 export default async function handler(request: Request) {
-  const { currentModel, message } = (await request.json()) as RequestData
+  const { currentModel, message } = (await request.json()) as RequestData;
 
   if (!message) {
-    return new Response('No message in the request', { status: 400 })
+    return new Response("No message in the request", { status: 400 });
   }
 
+  const reqMessages = message.map((item: string, index: number) => {
+    if (index % 2 === 0) {
+      return {
+        role: "user" as const,
+        content: item,
+      };
+    } else {
+      return {
+        role: "assistant" as const,
+        content: item,
+      };
+    }
+  });
+
+  const systemd = {
+    role: "system" as const,
+    content:
+      "あなたは万能のアシスタントAIです。質問に最適な回答を作ってください。",
+  };
+  console.log([systemd, ...reqMessages]);
+
   const payload: OpenAIStreamPayload = {
-    model: 'gpt-3.5-turbo',
+    model: "gpt-3.5-turbo",
     // model: `${currentModel}`,
-    messages: [{ role: 'user', content: message }],
+    messages: [systemd, ...reqMessages],
     temperature: 0.7,
     top_p: 1,
     frequency_penalty: 0,
@@ -29,8 +50,8 @@ export default async function handler(request: Request) {
     max_tokens: 200,
     stream: true,
     n: 1,
-  }
+  };
 
-  const stream = await OpenAIStream(payload)
-  return new Response(stream)
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
 }
